9a8ca4e1cee760999badec86cf9ccf579fff7d18
Fabian216 <fabian.alan.olaya@gmail.com>
2025-08-15 14:45:51 -0500

    first commit

---
+++ src/main/java/ntt/ntt_ms_customers/NttMsCustomersApplication.java
@@ -0,0 +1,13 @@
+package ntt.ntt_ms_customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttMsCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttMsCustomersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntt/ntt_ms_customers/NttMsCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package ntt.ntt_ms_customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttMsCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
